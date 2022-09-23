9554a7f783b792563579b9f5ab230a1567e1b13e
Sourish Chatterjee <sourishbaban@gmail.com>
2022-09-23 02:13:10 -0700

    fixing path

--- ritraj/include/js/main.js
+++ ritraj/include/js/main.js
@@ -222,7 +222,7 @@ BRUSHED.tweetFeed = function(){
 	var valueTop = -64; // Margin Top Value
 	
     $("#ticker").tweet({
-          modpath: './include/js/twitter/',
+          modpath: 'twitter/',
           username: "Bluxart", // Change this with YOUR ID
           page: 1,
           avatar_size: 0,
@@ -405,7 +405,7 @@ $(document).ready(function(){
 	Modernizr.load([
 	{
 		test: Modernizr.placeholder,
-		nope: './include/js/placeholder.js', 
+		nope: 'placeholder.js',
 		complete : function() {
 				if (!Modernizr.placeholder) {
 						Placeholders.init({
